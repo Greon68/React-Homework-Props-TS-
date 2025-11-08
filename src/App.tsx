9050c322cddc2b2a1bd8111e5b7e80fe7a333a5d
Greon68 <greon-ya@yandex.ru>
2025-11-08 18:17:38 +0300

    commit-0

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Products } from './components/Products/Products';
+
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <h3>Props + TS - Homework</h3>
+     
+      <Products/>
+    </> 
+  )
+}
+
+export default App
